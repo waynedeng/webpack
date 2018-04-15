7beeff6d0bc40f5bcc1aaa1d1a0b2660900166c3
wayne_deng <w.deng@rccchina.com>
2018-04-15 14:14:29 +0800

    "space-before-function-paren": 0,

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
@@ -69,6 +69,7 @@ module.exports = {
     }],
     {{/if_eq}}
     "vue/no-parsing-error": [2, { "x-invalid-end-tag": false }],
+    "space-before-function-paren": 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
